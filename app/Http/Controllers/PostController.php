1bd3aa043e89dd40011dcdba5346801c383f19f2
Regnars <k.regnars@gmail.com>
2024-10-17 14:19:36 +0300

    added a view, made a controller

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index(){
+
+        $posts = Post::all();
+        return view('posts.index', ['posts' => $posts]);
+    }
+
+
+    public function show($id){
+        
+        $post = Post::find($id);
+        //\Log::debug($id);
+        return view('posts.show', ['post' => $post]);
+
+    }
+
+
+
+}
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>All posts</h1>
+
+    <ul>
+        @foreach($posts as $post)
+        <li>
+            Title: {{ $post->title }}<br>
+            Content: {{ $post->content }}<br>
+            <a href="/posts"></a>
+        </li>
+        @endforeach
+    </ul>
+
+</body>
+</html>
